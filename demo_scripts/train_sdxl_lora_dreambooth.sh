0f63fdc8362f1f0d4f9b80267c21b70d300eb613
liuwenran <448073814@qq.com>
2023-11-29 15:51:47 +0800

    misc

--- demo_scripts/train_sdxl_lora_dreambooth.sh
+++ demo_scripts/train_sdxl_lora_dreambooth.sh
@@ -1,6 +1,6 @@
 export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
-export INSTANCE_DIR="/mnt/petrelfs/liuwenran/datasets/cctv/donghuatest/胡桃桃场景"
-export OUTPUT_DIR="work_dirs/cctv/lora-trained-xl-hutaotao-e4"
+export INSTANCE_DIR="/mnt/petrelfs/liuwenran/datasets/cctv/qianqiugesong/qianqiu/willow"
+export OUTPUT_DIR="work_dirs/cctv/lora-trained-xl-willow-e4"
 export VAE_PATH="madebyollin/sdxl-vae-fp16-fix"
 
 accelerate launch examples/dreambooth/train_dreambooth_lora_sdxl.py \
@@ -8,7 +8,7 @@ accelerate launch examples/dreambooth/train_dreambooth_lora_sdxl.py \
   --instance_data_dir=$INSTANCE_DIR \
   --pretrained_vae_model_name_or_path=$VAE_PATH \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of scene in cartoon style, ink painting" \
+  --instance_prompt="a photo in cartoon style, chinese cartoon, a man and a woman standing on a wooden boat, willow branches behind" \
   --mixed_precision="fp16" \
   --resolution=1024 \
   --train_batch_size=1 \
@@ -20,6 +20,6 @@ accelerate launch examples/dreambooth/train_dreambooth_lora_sdxl.py \
   --lr_warmup_steps=0 \
   --max_train_steps=310 \
   --checkpointing_steps=100 \
-  --validation_prompt="a photo of a building in cartoon style, ink painting" \
+  --validation_prompt="a photo in cartoon style, an old man" \
   --validation_epochs=30 \
   --seed="0"
