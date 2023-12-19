29d7c650e65b25f20d63c2f39461b96b5b2268e3
liuwenran <448073814@qq.com>
2023-12-19 19:47:09 +0800

    up

--- demo_scripts/infer_sd_xl_civitai_ckpt.py
+++ demo_scripts/infer_sd_xl_civitai_ckpt.py
@@ -7,8 +7,9 @@ import torch
 import os
 
 weight_dtype = torch.float16
-pretrained_model_name_or_path = "/mnt/petrelfs/liuwenran/models/civitai/ckpts/copaxTimelessxlSDXL1_v8.safetensors"
+# pretrained_model_name_or_path = "/mnt/petrelfs/liuwenran/models/civitai/ckpts/copaxTimelessxlSDXL1_v8.safetensors"
 # pretrained_model_name_or_path = '/mnt/petrelfs/liuwenran/models/civitai/ckpts/protovisionXLHighFidelity3D_release0630Bakedvae.safetensors'
+pretrained_model_name_or_path = '/mnt/petrelfs/liuwenran/models/civitai/ckpts/leosamsHelloworldSDXL_helloworldSDXL30.safetensors'
 
 pipeline = StableDiffusionXLPipeline.from_single_file(pretrained_model_name_or_path, torch_dtype=weight_dtype)
 # vae_path = "madebyollin/sdxl-vae-fp16-fix"
@@ -46,13 +47,18 @@ pipeline = pipeline.to("cuda")
 # prompt = 'A photo of a man riding camel in chinese ancient style, side view perspective, brown camel, perfect, extremely detailed, 8k'
 # prompt = 'photo of chinese young woman, highlight hair, sitting outside restaurant, wearing dress, rim lighting, studio lighting, looking at the camera, dslr, ultra quality, sharp focus, tack sharp, dof, film grain, Fujifilm XT3, crystal clear, 8K UHD, highly detailed glossy eyes, high detailed skin, skin pores'
 # prompt = 'oil painting, a chinese old house, sunshine, perfect, extremely detailed'
-prompt = 'a beautiful chinese woman, smiling, thin, pantyhose, black pantyhose, white sweater, black short skirt, black gloves, full-body photo, standing on snow ground, Gorgeous, charming, attractive, best quality, extremely detailed, sharp foucus, masterpeice, 8k, photorealistic, awesome, perfect'
+# prompt = 'a beautiful chinese woman, smiling, thin, pantyhose, black pantyhose, white sweater, black short skirt, black gloves, full-body photo, standing on snow ground, Gorgeous, charming, attractive, best quality, extremely detailed, sharp foucus, masterpeice, 8k, photorealistic, awesome, perfect'
+# prompt = 'leogirl,(pale skin, blush), (snow on stomach, snow on breasts, snow on legs), cold, highly detailed realistic photo of chinese beautiful (skinny) smiling woman standing in front of the (x-mas tree in busy city street:1.05), new year theme, Santa hat, nude and barefoot, (full body, feet), (winter, snow, frost, snowstorm:1.2), foggy evening, best quality, 8k, contrast lighting, detailed background,shot by iphone,'
+prompt = 'leogirl,(pale skin, blush),pink swimsuit, highly detailed realistic photo of chinese beautiful (skinny) smiling woman standing outside, barefoot, (full body, feet), (winter, snow, snow mountain), best quality, 8k, contrast lighting, detailed background,shot by iphone,'
+
 
 # negative_prompt = 'disfigured, ugly, bad, immature, cartoon, 3d, painting, b&w'
-negative_prompt = 'disfigured, ugly,bad legs, bad body, bad face, bad anatomy, bad hands, blur, painting, lowres,low quality, watermark, render, CG'
+# negative_prompt = 'disfigured, ugly,bad legs, bad body, bad face, bad anatomy, bad hands, blur, painting, lowres,low quality, watermark, render, CG'
+negative_prompt = '(worst quality,low resolution,bad hands,open mouth),distorted,twisted,watermark, disfigured, ugly,bad legs, bad body, bad face, bad anatomy, bad hands'
+
 generator = torch.Generator(device=torch.device('cuda')).manual_seed(1)
 
-folder_path = 'results/sd_xl_3d_copax/skiing_white_black_nudify'
+folder_path = 'results/sd_xl_3d_copax/skiing_white_black_nudify_goodp_helloworld'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
--- demo_scripts/train_sd15_lora_dreambooth.sh
+++ demo_scripts/train_sd15_lora_dreambooth.sh
@@ -1,38 +1,13 @@
-export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
-export INSTANCE_DIR="/mnt/petrelfs/liuwenran/datasets/cctv/donghuatest/胡桃桃场景"
-export OUTPUT_DIR="work_dirs/cctv/lora-trained-xl-hutaotao-e4"
-export VAE_PATH="madebyollin/sdxl-vae-fp16-fix"
+export MODEL_NAME="/mnt/petrelfs/liuwenran/.cache/huggingface/hub/models--Linaqruf--anything-v3.0/snapshots/8323d54dcf89c90c39995b04ae43166520e8992a"
+export INSTANCE_DIR="/mnt/petrelfs/liuwenran/datasets/cctv/qianqiugesong/qianqiu/willow"
+export OUTPUT_DIR="work_dirs/test-lora15"
 
-accelerate launch examples/dreambooth/train_dreambooth_lora_sdxl.py \
+accelerate launch examples/dreambooth/train_dreambooth_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
-  --pretrained_vae_model_name_or_path=$VAE_PATH \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of scene in cartoon style, ink painting" \
-  --mixed_precision="fp16" \
-  --resolution=1024 \
-  --train_batch_size=1 \
   --train_text_encoder \
-  --gradient_accumulation_steps=4 \
-  --learning_rate=1e-4 \
-  --report_to="wandb" \
-  --lr_scheduler="constant" \
-  --lr_warmup_steps=0 \
-  --max_train_steps=310 \
-  --checkpointing_steps=100 \
-  --validation_prompt="a photo of a building in cartoon style, ink painting" \
-  --validation_epochs=30 \
-  --seed="0"
-
-export MODEL_NAME="runwayml/stable-diffusion-v1-5"
-export INSTANCE_DIR="dog"
-export OUTPUT_DIR="path-to-save-model"
-
-accelerate launch train_dreambooth_lora.py \
-  --pretrained_model_name_or_path=$MODEL_NAME  \
-  --instance_data_dir=$INSTANCE_DIR \
-  --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of sks dog" \
+  --instance_prompt="a photo in chinese cartoon style, a man and a woman, willow" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
@@ -42,6 +17,6 @@ accelerate launch train_dreambooth_lora.py \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=500 \
-  --validation_prompt="A photo of sks dog in a bucket" \
+  --validation_prompt="A photo in chinese cartoon style, an old man" \
   --validation_epochs=50 \
   --seed="0" 
